0e2bc57fc67e0a235831c51811433ffa916e04aa
Miguel <miguelguzmane29@gmail.com>
2026-06-15 08:40:44 -0600

    Usa pool MySQL para evitar desconexion

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,30 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const mysql = require("mysql2");
 
-dotenv.config();
+require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
 
     if (err) {
-        console.log('Error de conexión:', err);
-    } else {
-        console.log('Base de datos conectada');
+        console.error("Error al conectar a la base de datos:", err);
+        return;
     }
+
+    console.log("Base de datos conectada");
+
+    conn.release();
 
 });
 
